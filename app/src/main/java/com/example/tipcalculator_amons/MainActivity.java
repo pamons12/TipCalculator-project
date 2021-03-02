a04839391df993163e0376bd1504d4950a662e09
pamons12 <47860735+pamons12@users.noreply.github.com>
2021-03-02 17:38:20 -0500

    Custom tip activity now working

--- app/src/main/java/com/example/tipcalculator_amons/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_amons/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tipcalculator_amons;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +12,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 0;
 
     private EditText checkTotalEditText;
     private EditText numOfPeopleEditText;
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Button customPercentButton;
     private Button resetButton;
     private Button calculateButton;
+
+    private Intent customTipIntent;
 
     private int checkTotal;
     private double tipPercent;
@@ -31,6 +38,25 @@ public class MainActivity extends AppCompatActivity {
         checkTotalEditText = findViewById(R.id.checkTotalEditText);
         numOfPeopleEditText = findViewById(R.id.numOfPeopleEditText);
 
+        customTipIntent = new Intent(this, customTipActivity.class);
+        customPercentButton = (Button) findViewById(R.id.customPerButton);
+        customPercentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(customTipIntent,REQUEST_CODE);
+            }
+        });
+
+        resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkTotal=0;
+                tipPercent=0;
+                numOfPeople=0;
+            }
+        });
+
         tenPercentButton = (Button) findViewById(R.id._10PerButton);
         tenPercentButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"10% Selected",Toast.LENGTH_SHORT).show();
             }
         });
+
         fifteenPercentButton = (Button) findViewById(R.id._15PerButton);
         fifteenPercentButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"15% Selected",Toast.LENGTH_SHORT).show();
             }
         });
+
         twentyPercentButton = (Button) findViewById(R.id._20PerButton);
         twentyPercentButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"20% Selected",Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle extras = data.getExtras();
+        tipPercent = extras.getDouble("tipPercent");
 
     }
 }
---
+++ app/src/main/java/com/example/tipcalculator_amons/customTipActivity.java
@@ -0,0 +1,62 @@
+package com.example.tipcalculator_amons;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class customTipActivity extends AppCompatActivity {
+    private EditText customTipEditText;
+    private Button confirmButton;
+    private double tipPercent;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_tip);
+
+        customTipEditText = (EditText) findViewById(R.id.customTipValue);
+
+        confirmButton = (Button) findViewById(R.id.confirmTipButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextReturnValue = customTipEditText.getText().toString();
+                if(customTipEditText.getText().toString().isEmpty()){
+                    showErrorAlert("Empty Percent Field",customTipEditText.getId());
+                } else {
+                    tipPercent = Double.parseDouble(customTipEditText.getText().toString());
+                    finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        Intent customTipIntent = new Intent();
+        customTipIntent.putExtra("tipPercent", tipPercent);
+        setResult(RESULT_OK, customTipIntent);
+        super.finish();
+    }
+
+    private void showErrorAlert(String errorMessage, final int fieldId) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(errorMessage)
+                .setNeutralButton("Close",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                findViewById(fieldId).requestFocus();
+                            }
+                        }).show();
+    }
+}
